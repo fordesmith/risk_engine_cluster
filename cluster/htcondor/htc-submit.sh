bda919c2ecf358d0cd161c47146c5c77eeb199a2
forde.a.smith@gmail.com <@kJaP#Me#n5RdB016R#A>
2020-10-19 19:14:03 +1100

    update -m option

--- cluster/htcondor/htc-submit.sh
+++ cluster/htcondor/htc-submit.sh
@@ -2,8 +2,8 @@
 echo $1 $2
 mkdir Input
 mkdir Market
-gsutil cp gs://risk_params/$1/$2/* ./
-gsutil cp gs://market_params/$1/* ./
+gsutil -m cp gs://risk_params/$1/$2/* ./
+gsutil -m cp gs://market_params/$1/* ./
 /home/condor/risk_engine/build/App/ore "./Input/ore.xml"
 gsutil cp ./Output/* gs://cpty_risk_outputs/$1/$2
 
