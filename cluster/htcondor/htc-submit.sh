05dbf841594eb24cb1c9b612c8e6299e02f67e02
forde.a.smith@gmail.com <@kJaP#Me#n5RdB016R#A>
2020-10-19 20:29:33 +1100

    try sudo

--- cluster/htcondor/htc-submit.sh
+++ cluster/htcondor/htc-submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #echo $1 $2
-mkdir Input
-mkdir Market
+sudo mkdir Input
+sudo mkdir Market
 #gsutil -m cp gs://risk_params/$1/$2/* ./
 #gsutil -m cp gs://market_params/$1/* ./
-gsutil -m cp gs://risk_params/09-10-20/cpty_01/* ./Input/
-gsutil -m cp gs://market_params/09-10-20/* ./Market/
-/home/condor/risk_engine/build/App/ore "./Input/ore.xml"
-gsutil cp ./Output/* gs://cpty_risk_outputs/09-10-20/cpty_01/
+sudo gsutil -m cp gs://risk_params/09-10-20/cpty_01/* ./Input/
+sudo gsutil -m cp gs://market_params/09-10-20/* ./Market/
+sudo /home/condor/risk_engine/build/App/ore "./Input/ore.xml"
+sudo gsutil cp ./Output/* gs://cpty_risk_outputs/09-10-20/cpty_01/
 
 # python3 start_job.py $1 $2ls
