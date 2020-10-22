1e9383e094876a70787a06f572a9378e9aaa2d89
forde.a.smith@gmail.com <@kJaP#Me#n5RdB016R#A>
2020-10-22 17:36:00 +1100

    working versions

--- risk_compiler/install_docker.sh
+++ risk_compiler/install_docker.sh
@@ -25,6 +25,7 @@ sudo usermod -aG docker fordesmith
 docker login --username=fordesmi
 
 docker build -t fordesmi/risk-engine-c7 .
+docker build -t test .
 
 
 # docker tag 23ef89761a82 fordesmi/pfe-poc-img:firsttry
@@ -35,30 +36,37 @@ docker push boost163_centos7
 docker push latest
 
 
-docker tag 331748446ea4 fordesmi/risk-engine-c7
+docker tag 47715e3c5d5c fordesmi/risk-engine-c7
 docker push fordesmi/risk-engine-c7
 
-docker run fordesmi/risk-engine-c7
+docker run -d fordesmi/risk-engine-c7
 
 docker logs d187fb45022e
 
-docker ps -a
+docker run -it -d fordesmi/risk-engine-c7
 
 
 docker stop 7d07e46157bd
 docker run -it -d fordesmi/risk-engine-c7
-docker attach 9ab849946216
+docker attach r02
 
 docker exec -d 85dcce91e677 stop
 
 docker run -it -d fordesmi/risk-engine-c7
 docker exec -d cd905c95fb74 cd /usr/local
-docker exec -d cd905c95fb74 mkdir Input
-docker exec -d cd905c95fb74 mkdir Market
-docker exec -d cd905c95fb74 gsutil -m cp gs://risk_params/09-10-20/cpty_01/* ./Input
-docker exec -d cd905c95fb74 gsutil -m cp gs://market_params/09-10-20/* ./Market
-docker exec -d 9ab849946216 /usr/local/risk_engine/build/App/ore "./Input/ore.xml"
-docker exec -d 9ab849946216 gsutil -m cp ./Output/* gs://cpty_risk_outputs/09-10-20/cpty_01/
+
+docker run --name r01 --rm -i -t fordesmi/risk-engine-v1:latest bash
+docker run --name r02 --rm -i -t -d fordesmi/risk-engine-v1:latest bash
+
+docker exec -d r02 bash /usr/local/run-risk-job.sh '09-10-20' 'cpty_01'
+
+docker exec -d r02 mkdir Input
+docker exec -d r02 mkdir Market
+docker exec -d r02 gsutil -m cp gs://risk_params/09-10-20/cpty_01/* ./Input
+docker exec -d r02 gsutil -m cp gs://market_params/09-10-20/* ./Market
+docker exec -d r02 /usr/local/risk_engine/build/App/ore "./Input/ore.xml"
+docker exec -d r02 gsutil -m cp ./Output/* gs://cpty_risk_outputs/09-10-20/cpty_01/
+
 gsutil cp ./Output/* gs://cpty_risk_outputs/$1/$2/
 
 export CLOUDSDK_PYTHON= /usr/bin/python3 /usr/bin/python3.6 /usr/bin/python3.6-config /usr/bin/python3.6m /usr/bin/python3.6m-config /usr/bin/python3.6m-x86_64-config /usr/lib/python3.6 /usr/lib64/python3.6 /usr/include/python3.6m
