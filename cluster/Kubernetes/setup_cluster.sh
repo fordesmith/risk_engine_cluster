e22d4c00f4294bd5792916ab223556d47101166c
forde.a.smith@gmail.com <@kJaP#Me#n5RdB016R#A>
2020-11-02 21:56:09 +1100

    update to working version

--- cluster/Kubernetes/setup_cluster.sh
+++ cluster/Kubernetes/setup_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gcloud container clusters create risk-cluster
 
-gcloud beta container --project "risk-engine-293410" clusters create "risk-cluster-clone-1" --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.16.13-gke.401" --machine-type "e2-highmem-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" --max-pods-per-node "110" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/risk-engine-293410/global/networks/default" --subnetwork "projects/risk-engine-293410/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+gcloud beta container --project "risk-engine-293410" clusters create "risk-cluster" --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.16.13-gke.401" --machine-type "e2-highmem-8" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" --max-pods-per-node "110" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/risk-engine-293410/global/networks/default" --subnetwork "projects/risk-engine-293410/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+
 gcloud container clusters get-credentials risk-cluster
 
 kubectl apply -f deployment.yaml
